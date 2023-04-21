d90fb6716deff27b9ecb8df60c842b6b3425f825
BetaTester772 <74451807+BetaTester772@users.noreply.github.com>
2023-04-21 11:42:54 +0900

    4/21

--- 3_31/04.R
+++ 3_31/04.R
@@ -3,10 +3,10 @@ q <- c(TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE)
 r <- c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE)
 
 lhs <- p &(q|r)
-rhs = (p&q) | (p&r)
+rhs <- (p&q) | (p&r)
 
 data.frame(p, q, r, lhs, rhs)
 
 
-x = 1: 1000
+x <- 1: 1000
 sum(which(x%%9==0))
